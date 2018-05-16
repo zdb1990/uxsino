0cbbe5d1c6366e0ac0d2fd028ac5f762bde4a0ea
zhangyao <zhang_yao@uxsino.com>
2018-05-16 18:14:55 +0800

    修改vscode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AutoComponent } from './auto/auto.component';
 
 const routes: Routes = [
-  { path: '', redirectTo:'/login',pathMatch:'full' },
-  {path:'login',component:AutoComponent}
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: AutoComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash: false})],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
